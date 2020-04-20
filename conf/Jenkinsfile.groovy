dfe8492bd0c91b25c37ea60278f684bc82ee6fc5
jenspettersson68 <mesmeratu@gmail.com>
2020-04-20 13:56:41 +0200

    disble stages

--- conf/Jenkinsfile.groovy
+++ conf/Jenkinsfile.groovy
@@ -9,20 +9,20 @@ pipeline {
         }
         stage('run script') {
             steps {
-               sh "./load_test_run.sh TODO_backend_test.jmx" 
+     //          sh "./load_test_run.sh TODO_backend_test.jmx" 
             }
         }
         stage('Publish report') {
             steps {
        // publish html
-            publishHTML target: [
-            allowMissing: false,
-            alwaysLinkToLastBuild: false,
-            keepAll: true,
-            reportDir: 'report',
-            reportFiles: 'index.html',
-            reportName: 'Performance Test Report'
-          ]
+       //     publishHTML target: [
+       //     allowMissing: false,
+       //     alwaysLinkToLastBuild: false,
+       //     keepAll: true,
+       //     reportDir: 'report',
+        //    reportFiles: 'index.html',
+       //     reportName: 'Performance Test Report'
+       //   ]
             }
         }
     }
